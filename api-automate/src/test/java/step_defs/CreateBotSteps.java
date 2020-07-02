6d8b1b95adf566ac6a62f20394a9855cafb3f408
vinay-pawar <vkpvinay016@gmail.com>
2020-07-02 17:15:13 +0530

    Enable Bot Working.
    Enable Bot Working. Logout Still not working

--- api-automate/src/test/java/step_defs/CreateBotSteps.java
+++ api-automate/src/test/java/step_defs/CreateBotSteps.java
@@ -16,7 +16,10 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
 import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import repos.APIResourceURI;
 
@@ -28,6 +31,7 @@ public class CreateBotSteps {
 	String cookie;
 	String workflow_id;
 	String csrf;
+	String xsrf;
 
 	@Given("base url {string} logs generated in {string} file")
 	public void base_url_as_https_automate_io_logs_generated_in_file(String host, String logFile) {
@@ -35,19 +39,22 @@ public class CreateBotSteps {
 		RestAssured.useRelaxedHTTPSValidation();
 		logger = Logger.getLogger(CreateBotSteps.class);
 		baseURL = host;
-		csrf = "41GPycMcFGZdDSnHibPBCnjo";
-		logger.info("\n==============================Starting Test Create Gmail Bot using API==============================");
+		csrf = "41GPycMcFGZdDSnHibPBCxyz";
+		xsrf = "HnM8Z2RG-ZzD6MNMiDszpFNmL20hAIsBRpkY";
+		logger.info("\n==============================Starting Test Create Gmail Bot using API==============================");		
 	}
 
 	@When("hit post method for login with arguments email: {string} and password: {string}")
 	public void hit_post_method_for_login_with_arguments_email_and_password(String email, String password) {
+		Header reqHeader = new Header("x-xsrf-token", xsrf);
 		logger.info("\n****************************************Login Process Initiated****************************************");
 		logger.info("Login URI: "+baseURL + APIResourceURI.LOGIN_URI);
 		logger.info("Request Payload: "+"{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}");
 		logger.info("Content-Type: "+ContentType.JSON);
 		logger.info("Accept: "+ContentType.JSON);
-		response = given().cookie("_csrf", csrf).contentType(ContentType.JSON).accept(ContentType.JSON)
-				.body("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}").cookie("_csrf", csrf).when()
+		logger.info("Header: { "+reqHeader.getName()+" : "+reqHeader.getValue()+" }");
+		response = given().cookie("_csrf", csrf).contentType(ContentType.JSON).accept(ContentType.JSON).header(reqHeader).header(new Header("_csrf", csrf))
+				.body("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}").cookie("_csrf", csrf).cookie("x-xsrf-token",xsrf).when()
 				.post(baseURL + APIResourceURI.LOGIN_URI);
 	}
 
@@ -69,29 +76,34 @@ public class CreateBotSteps {
 		String jsonBody = generateStringFromResource(
 				System.getProperty("user.dir") + "/src/test/java/requests/workspacerequest.json").replace("?", toEmail)
 						.replace("#", "" + (Math.random() * 99 + 1));
-		Header reqHeader = new Header("User-Agent",
+		Header reqHeader1 = new Header("User-Agent",
 				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+		Header reqHeader2 = new Header("x-xsrf-token", xsrf);
+		Headers headers = new Headers(reqHeader1,reqHeader2);
 		logger.info("Login URI: "+baseURL + APIResourceURI.WORKFLOW_URI);
 		logger.info("Request Payload: \n"+jsonBody);
 		logger.info("Content-Type: "+ContentType.JSON);
-		logger.info("Request Cookie: {_csrf : "+csrf);
-		logger.info("Request Cookie: {connect.sid : "+response.getCookie("connect.sid")+" }");
+		logger.info("Request Cookie: {_csrf : "+csrf+" }");
+		logger.info("Request Cookie: {connect.sid : "+cookie+" }");
 		logger.info("Accept: "+ContentType.JSON);
-		logger.info("Header: {"+reqHeader.getName()+" : "+reqHeader.getValue()+" }");
-		response = given().cookie("connect.sid", cookie).cookie("_csrf", csrf).contentType(ContentType.JSON)
-				.accept(ContentType.JSON).header(reqHeader).body(jsonBody).when()
+		logger.info("Headers: {"+reqHeader1.getName()+" : "+reqHeader1.getValue()+", "+reqHeader2.getName()+ " : "+reqHeader2.getValue()+" }");
+		response = given().cookie("connect.sid", cookie).cookie("_csrf",csrf).cookie("x-xsrf-token",xsrf).contentType(ContentType.JSON)
+				.accept(ContentType.JSON).headers(headers).body(jsonBody).when()
 				.post(baseURL + APIResourceURI.WORKFLOW_URI);
 	}
 
 	@Then("response code should be {int} ok and status will be {string} and message as {string}")
 	public void response_code_should_be_ok_and_status_will_be_success_and_message_as_Bot_successfully_stored(
 			int code, String status, String message) {
+		logger.info("Response Body: "+response.asString());
 		int actual_code=response.getStatusCode();
 		String actual_status=response.jsonPath().get("status");
 		Map<String, String> data = response.jsonPath().getMap("data");
+		if(data == null){
+			data = response.jsonPath().getMap("error");
+		}
 		workflow_id = data.get("_id");
-		String actual_message = ("message");
-		logger.info("Response Body: "+response.asString());
+		String actual_message = data.get("message");
 		logger.assertLog(code==actual_code, "Response status code mismatch. Expected: "+code+" Actual: "+actual_code);
 		logger.assertLog(status.equals(actual_status), "Response status mismatch. Expected: "+status+" Actual: "+actual_status);
 		logger.assertLog(message.equals(actual_message), "Response status mismatch. Expected: "+message+" Actual: "+actual_message);
@@ -101,17 +113,19 @@ public class CreateBotSteps {
 	@When("hit Get method to enable bot we created in previous step")
 	public void get_method_to_enable_bot_we_created_in_previous_step() {
 		logger.info("\n****************************************Bot Enabling Process Initiated****************************************");
-		Header reqHeader = new Header("User-Agent",
+		Header reqHeader1 = new Header("User-Agent",
 				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+		Header reqHeader2 = new Header("x-xsrf-token", xsrf);
+		Headers headers = new Headers(reqHeader1,reqHeader2);
 		logger.info("Login URI: "+baseURL + APIResourceURI.ENABLE_WORKFLOW_URI.replace("id", workflow_id));
 		logger.info("Content-Type: "+ContentType.JSON);
-		logger.info("Request Cookie: {_csrf : "+csrf);
-		logger.info("Request Cookie: {connect.sid : "+response.getCookie("connect.sid")+" }");
+		logger.info("Request Cookie: {_csrf : "+csrf+" }");
+		logger.info("Request Cookie: {connect.sid : "+cookie+" }");
 		logger.info("Accept: "+ContentType.JSON);
-		logger.info("Header: {"+reqHeader.getName()+" : "+reqHeader.getValue()+" }");
+		logger.info("Headers: {"+reqHeader1.getName()+" : "+reqHeader1.getValue()+", "+reqHeader2.getName()+ " : "+reqHeader2.getValue()+" }");
 		logger.info("Query Parameter: "+"skipPremiumCheck = true");
-		response = given().cookie("connect.sid", cookie).cookie("_csrf", csrf).contentType(ContentType.JSON)
-				.accept(ContentType.JSON).header(reqHeader).queryParam("skipPremiumCheck", true).when()
+		response = given().cookie("connect.sid", cookie).cookie("_csrf", csrf).cookie("x-xsrf-token",xsrf).contentType(ContentType.JSON)
+				.accept(ContentType.JSON).headers(headers).queryParam("skipPremiumCheck", true).when()
 				.post(baseURL + APIResourceURI.ENABLE_WORKFLOW_URI.replace("id", workflow_id));
 	}
 
@@ -122,7 +136,10 @@ public class CreateBotSteps {
 		int actual_code=response.getStatusCode();
 		String actual_status=response.jsonPath().get("status");
 		Map<String, String> data = response.jsonPath().getMap("data");
-		String actual_message = ("message");		
+		if(data == null){
+			data = response.jsonPath().getMap("error");
+		}
+		String actual_message = data.get("message");		
 		logger.assertLog(code==actual_code, "Response status code mismatch. Expected: "+code+" Actual: "+actual_code);
 		logger.assertLog(status.equals(actual_status), "Response status mismatch. Expected: "+status+" Actual: "+actual_status);
 		logger.assertLog(message.equals(actual_message), "Response status mismatch. Expected: "+message+" Actual: "+actual_message);
@@ -132,29 +149,29 @@ public class CreateBotSteps {
 	@Then("logout the session and verify response code should be {int} ok and status will be {string} and message as {string}")
 	public void logout_the_session_and_verify_message_Session_expired(int code, String status, String message) {
 		logger.info("\n****************************************Logout Process Initiated****************************************");
-		Header reqHeader = new Header("User-Agent",
+		Header reqHeader1 = new Header("User-Agent",
 				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+		Header reqHeader2 = new Header("x-xsrf-token", xsrf);
+		Headers headers = new Headers(reqHeader1,reqHeader2);
 		logger.info("Login URI: "+baseURL + APIResourceURI.LOGOUT_URI);
 		logger.info("Content-Type: "+ContentType.JSON);
 		logger.info("Request Cookie: {_csrf : "+csrf);
-		logger.info("Request Cookie: {connect.sid : "+response.getCookie("connect.sid")+" }");
+		logger.info("Request Cookie: {connect.sid : "+cookie+" }");
 		logger.info("Accept: "+ContentType.JSON);
-		logger.info("Header: {"+reqHeader.getName()+" : "+reqHeader.getValue()+" }");
-		response = given().cookie("connect.sid", cookie).cookie("_csrf", csrf).contentType(ContentType.JSON)
-				.accept(ContentType.JSON).header(reqHeader).when().delete(baseURL + APIResourceURI.LOGOUT_URI);
+		logger.info("Headers: {"+reqHeader1.getName()+" : "+reqHeader1.getValue()+", "+reqHeader2.getName()+ " : "+reqHeader2.getValue()+" }");
+		response = given().cookie("connect.sid", cookie).cookie("_csrf", csrf).contentType(ContentType.JSON).cookie("x-xsrf-token",xsrf).header(new Header("-csrf", csrf))
+				.accept(ContentType.JSON).headers(headers).when().delete(baseURL + APIResourceURI.LOGOUT_URI);
 		int actual_code=response.getStatusCode();
 		String actual_status=response.jsonPath().get("status");
 		Map<String, String> data = response.jsonPath().getMap("data");
-		String actual_message = ("message");
+		if(data == null){
+			data = response.jsonPath().getMap("error");
+		}
+		String actual_message = data.get("message");
 		logger.info("Response Body: "+response.asString());
 		logger.assertLog(code==actual_code, "Response status code mismatch. Expected: "+code+" Actual: "+actual_code);
 		logger.assertLog(status.equals(actual_status), "Response status mismatch. Expected: "+status+" Actual: "+actual_status);
 		logger.assertLog(message.equals(actual_message), "Response status mismatch. Expected: "+message+" Actual: "+actual_message);
-		/*if (response.getStatusCode() == 200) {
-			logger.info("Logout Successfully");
-		} else {
-			logger.info("logout fail due to following error :\n" + response.asString());
-		}*/
 		logger.info("\n****************************************Logout Process Ended****************************************");
 	}
 
